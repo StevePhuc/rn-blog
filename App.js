dfaa2591ed19e8cb4e24ed58a1c702f8b2ef325e
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-27 20:55:30 +0200

    122. Wrapping the Navigator

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+import React from 'react'
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import IndexScreen from './scr/screens/indexScreen'
@@ -11,4 +12,8 @@ const navigation = createStackNavigator({
   }
 })
 
-export default createAppContainer(navigation)
+const App = createAppContainer(navigation)
+
+export default () => {
+  return <App />
+} 
