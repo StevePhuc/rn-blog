8428a0bc576bb411a8d2877c4ccb46b90f6c1226
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-27 21:33:35 +0200

    125. Moving Data with Context

--- scr/screens/indexScreen.js
+++ scr/screens/indexScreen.js
@@ -1,14 +1,15 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useContext } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import BlogContext from "../context/BlogContext";
 
 export default function App() {
+    const value = useContext(BlogContext);
     return (
-        <View >
+        <View>
             <Text>Index Screen</Text>
+            <Text>{value}</Text>
         </View>
     );
 }
 
-const styles = StyleSheet.create({
-
-});
+const styles = StyleSheet.create({});
