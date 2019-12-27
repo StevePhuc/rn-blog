e3b9809b797d4baa24dd88c37d56428b60365664
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-27 21:11:23 +0200

    124. Adding Context

--- App.js
+++ App.js
@@ -1,19 +1,27 @@
-import React from 'react'
-import { createAppContainer } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
-import IndexScreen from './scr/screens/indexScreen'
+import React from "react";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./scr/screens/indexScreen";
+import { BlogProvider } from "./scr/context/BlogContext";
 
-const navigation = createStackNavigator({
-  Index: IndexScreen
-}, {
-  initialRouteName: 'Index',
-  defaultNavigationOptions: {
-    title: 'Blogs'
-  }
-})
+const navigation = createStackNavigator(
+    {
+        Index: IndexScreen
+    },
+    {
+        initialRouteName: "Index",
+        defaultNavigationOptions: {
+            title: "Blogs"
+        }
+    }
+);
 
-const App = createAppContainer(navigation)
+const App = createAppContainer(navigation);
 
 export default () => {
-  return <App />
-} 
+    return (
+        <BlogProvider>
+            <App />
+        </BlogProvider>
+    );
+};
