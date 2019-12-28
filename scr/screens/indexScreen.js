aa466817e7d5fcea9a445cc9a994a540c3079df2
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-28 07:44:26 +0200

    133. A Bit of Styling

--- scr/screens/indexScreen.js
+++ scr/screens/indexScreen.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View, FlatList, Button } from "react-native";
 import { Context as BlogContext } from "../context/BlogContext";
+import { Feather } from "@expo/vector-icons";
 
 export default function App() {
     const { state, addBlogPost } = useContext(BlogContext);
@@ -13,11 +14,31 @@ export default function App() {
                 data={state}
                 keyExtractor={blogPost => blogPost.title}
                 renderItem={({ item }) => {
-                    return <Text>{item.title}</Text>;
+                    return (
+                        <View style={styles.row}>
+                            <Text style={styles.title}>{item.title}</Text>
+                            <Feather style={styles.icon} name="trash" />
+                        </View>
+                    );
                 }}
             />
         </View>
     );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: "gray"
+    },
+    title: {
+        fontSize: 18
+    },
+    icon: {
+        fontSize: 24
+    }
+});
