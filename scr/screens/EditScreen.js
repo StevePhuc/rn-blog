0c4c0942b70fc69392640ec688cf78e37dbf0d9c
Steve Phuc <stevephuc.dev@gmail.com>
2019-12-30 19:51:05 +0200

    145. Initializing State from Context

--- scr/screens/EditScreen.js
+++ scr/screens/EditScreen.js
@@ -1,16 +1,37 @@
-import React, { useContext } from "react";
-import { View, Text, TouchableOpacity } from "react-native";
+import React, { useContext, useState } from "react";
+import { View, Text, TextInput, TouchableOpacity, StyleSheet } from "react-native";
+import { Context } from "../context/BlogContext";
 
 const EditScreen = ({ navigation }) => {
-
-
+    const { state } = useContext(Context);
+    const blogPost = state.find(blogPost => blogPost.id === navigation.getParam("id"));
+    const [title, setTitle] = useState(blogPost.title);
+    const [content, setContent] = useState(blogPost.content);
 
     return (
         <View>
-            <Text>Edit Screen - {navigation.getParam('id')}</Text>
+            <Text style={styles.label}>Enter Title:</Text>
+            <TextInput style={styles.input} value={title} onChangeText={text => setTitle(text)} />
         </View>
     );
 };
+
+
+const styles = StyleSheet.create({
+    input: {
+        fontSize: 18,
+        borderWidth: 1,
+        borderColor: "black",
+        marginBottom: 15,
+        padding: 5,
+        margin: 5
+    },
+    label: {
+        fontSize: 20,
+        marginBottom: 10,
+        marginLeft: 5
+    }
+});
 
 
 export default EditScreen;
